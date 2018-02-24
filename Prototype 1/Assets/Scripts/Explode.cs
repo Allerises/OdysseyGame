79009c59e13747013e9b9c72165459df87c3cf87
Allerises <ibby.star.111@gmail.com>
2018-02-24 13:45:16 +0700

    Prototype1 With Prefabs

---
+++ Prototype 1/Assets/Scripts/Explode.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explode : MonoBehaviour {
+
+	Rigidbody rb;
+	public GameObject obj, ExplosionCollider;
+	public float explosionForce, mod, radius, force;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		rb.velocity *= force;
+		ExplosionCollider.SetActive(false);
+	}
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.E)) {
+			transform.parent = null;
+			ExplosionCollider.SetActive(true);
+			rb.AddExplosionForce(explosionForce, transform.position, radius, mod);
+		}
+	}
+
+	void OnTriggerEnter (Collider other) {
+		rb.velocity = Vector3.zero;
+		rb.useGravity = false;
+		transform.parent = other.transform;
+		transform.rotation = Quaternion.identity;
+	}
+
+	void OnTriggerExit (Collider other) {
+		Destroy(obj);
+	}
+}
---
+++ Prototype 1/Assets/Scripts/ShootProjectile.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShootProjectile : MonoBehaviour {
+
+	public GameObject[] loadout;
+	public Transform other;
+	public float force, offset;
+	public Text UI;
+	int i;
+
+	void Start () {
+		i = 0;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.X)) {
+			i++;
+
+			switch (i) {
+			case 0:
+				UI.text = "Ball";
+				break;
+			case 1:
+				UI.text = "Explosive";
+				break;
+			}
+
+			if (i == loadout.Length) {
+				i = 0;
+			}
+		}
+
+		Vector3 dir = (transform.position - other.transform.position).normalized;
+		if (Input.GetKeyDown(KeyCode.F)) {
+			Debug.DrawLine(transform.position, other.position, Color.red, 1);
+			Rigidbody rb = Instantiate(
+			                   loadout[i],
+			                   new Vector3(
+			                       transform.position.x,
+			                       transform.position.y,
+			                       transform.position.z + offset),
+			                   Quaternion.identity).GetComponent<Rigidbody>();
+			rb.velocity = dir * force;
+		}
+	}
+}
