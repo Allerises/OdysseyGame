8dd27cf1f5e65e9b12c83af9c66d67d1c5b45083
John <hahn30761@sas.edu.sg>
2018-03-07 17:55:32 +0800

    Sound Effects + Menu (Try 2)

---
+++ TeamOdyssey_Game/Assets/Scripts/Demolished.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Demolished : MonoBehaviour {
+
+	bool demolished;
+	public float height;
+
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y < height) {
+			demolished = true;
+		} else {
+			demolished = false;
+		}
+	}
+
+	public bool isDemolished () {
+		return demolished;
+	}
+}
---
+++ TeamOdyssey_Game/Assets/Scripts/Explode.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explode : MonoBehaviour {
+
+	Rigidbody rb;
+	public GameObject obj, ExplosionCollider;
+	public float explosionForce, mod, radius, force;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		rb.velocity *= force;
+		ExplosionCollider.SetActive(false);
+	}
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.E)) {
+			transform.parent = null;
+			ExplosionCollider.SetActive(true);
+			rb.AddExplosionForce(explosionForce, transform.position, radius, mod);
+			Destroy(obj, 0.1f);
+		}
+	}
+
+	void OnTriggerEnter (Collider other) {
+		rb.velocity = Vector3.zero;
+		rb.useGravity = false;
+		transform.parent = other.transform;
+		transform.rotation = Quaternion.identity;
+	}
+}
